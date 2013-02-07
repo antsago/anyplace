b1ad464e44755b63aa0724f469d3d828581596dd
antsago <antsago@ymail.com>
2013-02-07 17:32:10 +0000

    Updated file to use country ID
    Again necessary to check name of country id column and maybe also its position (not sure if it's important)

--- InputCity.php
+++ InputCity.php
@@ -1,9 +1,10 @@
 //Needs an html form that post name and description of the city and id of country
 <?php
   
-  //get name and description
+  //get name, description and id of country
   $cName = $_POST["name"];
   $cDescription = $_POST["description"];
+  $countryID = $_POST["countryID"];
   
   //connect database
   mysql_connect("ramen.cs.man.ac.uk", "12_COMP10120_B1", "ztDsBWSMqDny80BR")
@@ -15,14 +16,14 @@
 
   if (cityIsNew($cName))
   {
-    mysql_query("INSERT INTO Cities (City Name, Description)
-                 VALUES ('$cName', '$cDescription')");
+    mysql_query("INSERT INTO Cities (CountryID/*check name*/, City Name, Description)
+                 VALUES ('$countryID', '$cName', '$cDescription')");
   }//if
   
   
 //--------functions-------//
   
-  //Check if a city exists
+  //Check if a city exists, if lots of cities improve function using country id
   function cityIsNew($cName)
   {
     //fetch list of countries
