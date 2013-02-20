11d7bd65548caa9934cf2d025b932bec97b98c88
Savvas Ashiotis <ashiots2@gmail.com>
2013-02-20 11:27:58 +0000

    Update CityPage.php

--- CityPage.php
+++ CityPage.php
@@ -1,33 +1,29 @@
 //Page to use InputCity.php
+<!DOCTYPE HTML>
 <html>
-<body>
+
+
+<form action="inputCity.php" method="post">
+<form>
+<select name="countryID">
 <?php
   //connect database
-  mysql_connect("ramen.cs.man.ac.uk", "12_COMP10120_B1", "ztDsBWSMqDny80BR")
-    or die('Could not connect: ' . mysql_error());
-  mysql_select_db("12_COMP10120_B1", $connection) or die('Could not connect to '.
-                                          'the database: '.mysql_error());
+  require('connectToDatabase.php');
 
-  //function structure gotten from http://goo.gl/GIXj6
-  $result = mysql_query("SELECT ID?, County name? FROM County?"); 
-
-  //Variable to store the countries
-  $options = ""; 
-  while ($row=mysql_fetch_array($result)) 
-  { 
-    $id = $row["ID"]; 
-    $countryName = $row["Country Name?"]; 
-    $options .= "<option value=\"$id\">$countyName</option>"; 
-  }//while 
-?> 
-<form method="post" action="InputCity.php">
-  <select name="countyID">
-    <option disabled selected>Choose a Country</option>
-    <?php $options>
-  </select><br>
-  City Name: <input type="text" name="name"><br>
-  Description: <input type="textarea" name="description"><br>
-  <input type="submit" value"Submit">
+  $result =mysql_query("SELECT CountryID, Name FROM Countries");
+  while ($row=mysql_fetch_assoc($result))
+  {?>
+    <option value="<?php echo $row['CountryID']?>"><?php echo $row['Name']?></option>
+  <?php }//while
+  require('closeConnection.php');
+?>
+<option disabled selected>Choose a Country</option>
+</select>
+Name: <input type="text" name="cityName" /> <br>
+Description: <textarea rows="10" cols="30" name="description"> </textarea>
+<input type="submit" />
 </form>
+
+
 </body>
 </html>
