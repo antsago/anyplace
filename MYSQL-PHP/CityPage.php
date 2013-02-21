4f7a0e2bad5b1b639189e8d719ef20e739a5e0b0
Antonio Sanchez Gomez <sago.ant@gmail.com>
2013-02-21 16:46:10 +0000

    All the files readed (all are all)

---
+++ MYSQL-PHP/CityPage.php
@@ -0,0 +1,28 @@
+//Page to use InputCity.php
+<!DOCTYPE HTML>
+<html>
+
+
+<form action="inputCity.php" method="post">
+<select name="countryID">
+<?php
+  //connect database
+  require('connectToDatabase.php');
+
+  $result =mysql_query("SELECT CountryID, Name FROM Countries");
+  while ($row=mysql_fetch_assoc($result))
+  {?>
+    <option value="<?php echo $row['CountryID']?>"><?php echo $row['Name']?></option>
+  <?php }//while
+  require('closeConnection.php');
+?>
+<option disabled selected>Choose a Country</option>
+</select>
+Name: <input type="text" name="cityName" /> <br>
+Description: <textarea rows="10" cols="30" name="description"> </textarea>
+<input type="submit" />
+</form>
+
+
+</body>
+</html>
---
+++ MYSQL-PHP/CountryForm.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+
+<form action="inputCountry.php" method="post">
+Name: <input type="text" name="countryName" /> <br>
+Description: <textarea rows="10" cols="30" name="description"> </textarea>
+<input type="submit" />
+</form>
+
+</body>
+</html>
