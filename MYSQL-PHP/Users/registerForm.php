a48841cfa2b45cf4a642cd0328351d914207b60a
Antonio Sanchez Gomez <sago.ant@gmail.com>
2013-02-26 23:35:33 +0000

    Added register and some login

--- MYSQL-PHP/Users/registerForm.php
+++ MYSQL-PHP/Users/registerForm.php
@@ -1,8 +1,9 @@
 <html>
 <body>
-<form action="" method="post">
+<form action="registerInput.php" method="post">
   Name: <input type="text" name="Name" autofocus/> <br>
   Surname: <input type="text" name="Surname"/> <br>
+  <!--email will be computed in future versions-->
   Email: <input type="email" name="Email"/> <br>
   Password <input type="password" name="Password"/> <br>
   <select name="UniversityID">
@@ -22,11 +23,8 @@
   Age: <input type="number" name="age"/> <br>
   <input type="radio" name="gender" value="male">Male   
   <input type="radio" name="gender" value="female">Female<br>
-  <select name="CountryID"> 
+  <select name="countryID"> 
     <?php
-      //connect database
-      require('connectToDatabase.php');
-      
       $result =mysql_query("SELECT CountryID, Name FROM Countries");
       
       echo "<option selected disabled> Choose your country </option>";
@@ -34,6 +32,8 @@
       {
 	echo "<option value=".$row['CountryID'].">".$row['Name']."</option>";
       }//while
+      
+      //close connection (still no in github)
     ?>
   </select> <br>
 <input type="submit"/>
