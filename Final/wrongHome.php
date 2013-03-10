809a00c66071bd4d5b9b26899dc665d801465c98
Antonio Javier Sanchez Gomez <sanchea2@rs0.(none)>
2013-03-10 16:28:14 +0000

    deleted connect and close in search

---
+++ Final/wrongHome.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<body background="images/lightblue.jpg">
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="utf-8">
+<h1>
+@nyplace
+</h1>
+<header>
+<table border="0">
+<div>
+<tr>
+<td>
+<a class="now"  href="home.php">
+<img src="images/logo.png"
+     width="200" height="100"></a>     
+</td><td >
+
+<a class="list" href="search/search.php">
+<img border="0" src="images/Search.png"
+     width="200" height="100"></a>
+</td><td>
+
+<a class="list" href="hosting/hosting.php">
+<img border="0" src="images/Hosting.png"
+     width="200" height="100"></a>
+</a>
+</td>
+</tr>
+</table>
+</div>
+</header>
+
+
+<table border="1">
+<tr>
+<form action="login/login1.php" method="post">
+<table border="0">
+<tr><td>
+ <p color="red">Wrong email or password</p>
+</td></tr>
+<tr><td>
+  Email: 
+</td><td>
+  <input type="email" name="Email"/> <br>
+</td></tr><tr><td>
+  Password: 
+</td><td>
+<input type="password" name="password"/> <br>
+</td></tr><tr><td></td><td>
+<input style="float: right;" type="submit" value="fly"/>
+</td>
+</table>
+</form>
+
+<p>
+<right>
+ Not a User? 
+<a href="http://soba.cs.man.ac.uk/~sanchea2/Page_design/register.php"> 
+Register</a>
+</p>
+</right>
+</form>
+
+
+
+</td>';}//else 
+?>
+<td>
+<marquee behavior="scroll" direction="left">
+
+<img border="0" src="images/photo1.jpg" alt=""
+     hieght="250" width="304">
+<img border="0" src="images/photo2.jpg" alt=""
+     hieght="250" width="304">
+<img border="0" src="images/photo3.jpg" alt=""
+     hieght="250" width="304">
+<img border="0" src="images/photo4.jpg" alt=""
+     hieght="250" width="304">      
+</marquee>
+</td>
+</tr> 
+</table>
+
+<table border="1">  
+<tr>
+<td>
+forums updates~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+</td>
+<td>
+photo updates~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+</td>
+</tr>
+</table>
+
+<footer>
+<nav>
+<a class="foot" href="textOnly/contactUs.php">
+<strong>Contact Us</strong></a>
+<a class="foot" href="textOnly/aboutUs.php">
+<strong>About Us</strong></a>
+<a class="foot" href="textOnly/disclaimer.php">
+<strong>Disclaimer</strong></a>
+</nav>
+</footer>
+
+
+
+</body>
+</html>
